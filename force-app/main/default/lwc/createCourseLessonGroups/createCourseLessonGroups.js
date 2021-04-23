7cec8425d68166184b0fa255bb884811b7b3cf66
kirill.terentyev <kirill.terentyev@synebo.io>
2021-04-23 19:56:15 +0300

    multipicklist for groups that attend course implemented

--- force-app/main/default/lwc/createCourseLessonGroups/createCourseLessonGroups.js
+++ force-app/main/default/lwc/createCourseLessonGroups/createCourseLessonGroups.js
@@ -1,8 +1,21 @@
-import { api, LightningElement } from 'lwc';
+import { api, track, LightningElement } from 'lwc';
 
 export default class CreateCourseLessonGroups extends LightningElement {
 
     @api lessonId;
+
+    @track availableGroups = [
+        { label: '3-1', value: '1' },
+        { label: 'НАИ-172у', value: '2' },
+        { label: 'АИ-176', value: '3' }
+    ];
+
+    @track attendingGroups = [ ];
+
+    handleGroupChange(event) {
+        const selectedOptionsList = event.detail.value;
+        console.log(`Options selected: ${selectedOptionsList}`);
+    }
 
     @api
     handleSave() {
